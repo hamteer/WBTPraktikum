86c4ab6f3f5cf344e2512a4d5148173648811e95
hamteer <philham.001@gmail.com>
2021-11-24 15:01:16 +0100

    repository saubergemacht

--- js/chat.js
+++ js/chat.js
@@ -1,89 +1,156 @@
-    
-    
-        
-window.chatToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjM2NjU0MzMwfQ.t0AIqQIzxGgIqhPMPUCIFZiiTQE1BpPJEkEe8YsmxSY";
-window.chatCollectionId = "b5eb31e8-e269-4c8a-9c86-e99c3a992f51";
-window.chatServer = "https://online-lectures-cs.thi.de/chat";
+const sendBtn = document.getElementById("sendBtn");
+// get ul element for append
+const ul = document.getElementById("chatList");
+// get message from text input
+const msgInput = document.getElementById("inputMsg");
 
-
-var xmlhttp = new XMLHttpRequest();
-
-// Daten holen und in list einfügen
-xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        let data = JSON.parse(xmlhttp.responseText);
-        console.log(data);
-        const l = data.length - 1;
-        console.log(data[l].msg);
-
-        let msg = data[l].msg;
-        //document.getElementById('test').innerHTML = (data[l - 2].from) + ': ' + (data[l - 2].msg) + '<br>' + (data[l - 1].msg) + '<br>' + (data[l].msg);
-        document.getElementById('firstFrom').innerHTML = data[l].from + ': ';
-        document.getElementById('firstTxt').innerHTML = data[l].msg;
-        var fTimestamp = data[l].time;
-        var fDate = new Date(fTimestamp); 
-        document.getElementById('firstTime').innerHTML = " "+fDate.getHours() + ":" + fDate.getMinutes() + ":" + fDate.getSeconds();
-
-        document.getElementById('secondFrom').innerHTML = data[l - 1].from + ': ';
-        document.getElementById('secondTxt').innerHTML = data[l - 1].msg;
-        var sTimestamp = data[l - 1].time;
-        var sDate = new Date(sTimestamp); 
-        document.getElementById('secondTime').innerHTML = " "+sDate.getHours() + ":" + sDate.getMinutes() + ":" + sDate.getSeconds();
-
-        document.getElementById('thirdFrom').innerHTML = data[l - 2].from + ': ';
-        document.getElementById('thirdTxt').innerHTML = data[l - 2].msg;
-        var tTimestamp = data[l - 2].time;
-        var tDate = new Date(tTimestamp); 
-        document.getElementById('thirdTime').innerHTML = " "+tDate.getHours() + ":" + tDate.getMinutes() + ":" + tDate.getSeconds();
-
-        
-    }
-};
-
-
-//Daten senden mit onclick auf Button
-function handleKnopf() {
-
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 204) {
-            console.log("done...");
-            //console.log(data);
-        } else {
-            //console.log("request fehlgeschlagen");
-        }
-    };
-
-
-    xmlhttp.open("POST", "https://online-lectures-cs.thi.de/chat/b5eb31e8-e269-4c8a-9c86-e99c3a992f51/message", true);
-    xmlhttp.setRequestHeader('Content-type', 'application/json');
-    // Add token, e. g., from Tom
-    xmlhttp.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjM2NjU0MzMwfQ.t0AIqQIzxGgIqhPMPUCIFZiiTQE1BpPJEkEe8YsmxSY');
-    // Create request data with message and receiver
-
-
-    var eingabe = document.getElementById('eg').value;
-    //console.log(eingabe);
-
-    let data = {
-        message: document.getElementById('eg').value,
-        to: "Jerry"
-    };
-    console.log(data);
-
-    let jsonString = JSON.stringify(data); // Serialize as JSON
-    console.log(jsonString);
-    xmlhttp.send(jsonString); // Send JSON-data to server
-
+//
+// create all necessary elements for one Message
+//
+function createMessageElementinChat(msgTxt, sender, time) {
+    // create single message elements: li whole message
+    const li = document.createElement("li");
+    li.classList.add("message");
+    // create single message elements: p message container
+    const p = document.createElement("p");
+    p.classList.add("messageContainer");
+    // create single message elements: span chatName name of sender
+    const spName = document.createElement("span");
+    spName.classList.add("chatName");
+    spName.textContent = sender + ": ";
+    // create single message elements: span chatMessage
+    const spMsg = document.createElement("span");
+    spMsg.classList.add("chatMessage");
+    spMsg.textContent = msgTxt;
+    // create single message elements: span timestamp
+    const spTime = document.createElement("span");
+    spTime.classList.add("chatTimestamp");
+    spTime.textContent = time;
+    // p append children chatName, chatMessage, timestamp
+    p.appendChild(spName);
+    p.appendChild(spMsg);
+    p.appendChild(spTime);
+    // li append p
+    li.appendChild(p);
+    // ul append complete Message
+    ul.appendChild(li);
 }
 
-//Zeitschleife um Daten zu aktualisieren
-window.setInterval(function () {
 
+
+//
+// Data-section: get, process and send data
+//
+function getAndProcessData() {
+    let xmlhttp = new XMLHttpRequest();
+    // get data
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            let data = JSON.parse(xmlhttp.responseText);
+            let length = data.length;
+            clearList();
+            processData(data, length);
+        }
+    }
     xmlhttp.open("GET", "https://online-lectures-cs.thi.de/chat/b5eb31e8-e269-4c8a-9c86-e99c3a992f51/message/Jerry", true);
     xmlhttp.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjM2NjU0MzMwfQ.t0AIqQIzxGgIqhPMPUCIFZiiTQE1BpPJEkEe8YsmxSY');
     xmlhttp.send();
+}
 
-}, 1000)
+// process incoming data and create message-elements
+function processData(data, length) {
+    for(let i = 0; i < length; i++) {
+        const timestamp = formatTime(data[i].time);
+        createMessageElementinChat(data[i].msg, data[i].from, timestamp);
+    }
+}
+
+// clear ul list 
+function clearList(){
+    while (ul.firstChild) {
+        ul.removeChild(ul.lastChild);
+      }
+}
+
+// format ms to hh:mm:ss
+function formatTime(time) {
+    const fDate = new Date(time);
+    let hours = fDate.getHours();
+    let minutes = fDate.getMinutes();
+    if(Number(minutes) < 10) {
+        minutes = "0" + fDate.getMinutes();
+    }
+    let seconds = fDate.getSeconds();
+    if(Number(seconds) < 10) {
+        seconds = "0" + fDate.getSeconds();
+    }
+    return hours + ":" + minutes + ":" + seconds;
+}
+
+// bind and send new messages
+function sendMessage() {
+    // if msgInput is not empty send Message else do nothing
+    if (msgInput.value.length !== 0) {
+        let xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 204) {
+                console.log("message sending successful");
+            } else {
+                console.log("message sending failed");
+            }
+        };
+    
+        xmlhttp.open("POST", "https://online-lectures-cs.thi.de/chat/b5eb31e8-e269-4c8a-9c86-e99c3a992f51/message", true);
+        xmlhttp.setRequestHeader('Content-type', 'application/json');
+        // Add token, e. g., from Jerry because we are sending it to Tom
+        xmlhttp.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiSmVycnkiLCJpYXQiOjE2MzY2NTQzMzB9.4bbvYgnUfMbmMSyk50LlgVR-2mNPF5db9khiP_SZ5hM');
+
+        //prepare message-package
+        let data = {
+            // get message value from msg-element declarated at the top of the file
+            message: msgInput.value,
+            to: "Tom"
+        };
+
+        // create a JSON-String
+        const jsonString = JSON.stringify(data); 
+
+        // Send JSON-data to server
+        xmlhttp.send(jsonString);
+    } 
+}
+
+//
+//  event handling
+//
+// handling button click event
+sendBtn.addEventListener("click", function () {
+        sendMessage();
+        updateWindow();
+        msgInput.value = "";
+});
+// handling 'enter'-pressed in message input
+msgInput.addEventListener("keypress", function(e) {
+    if (e.key == 'Enter') {
+        sendMessage();
+        updateWindow();
+        msgInput.value = "";
+    }
+});
+
+//
+//  update window
+//
+window.setInterval(updateWindow, 1000);
+
+function updateWindow(){
+    getAndProcessData();
+}
+
+updateWindow();
+
+
+
+
 
 
