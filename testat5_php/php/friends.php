3af9aeb44e9d2d944e4b356d6440197a081de9eb
STRG-C-Coder <81913916+STRG-C-Coder@users.noreply.github.com>
2021-12-14 19:08:54 +0100

    Add files via upload
    add friends.php

--- testat5_php/php/friends.php
+++ testat5_php/php/friends.php
@@ -1,8 +1,66 @@
-<!DOCTYPE html>
-<html>
 <?php
     require 'start.php';
+    
+    if (!isset($_SESSION['user'])) {
+        header('location: login.php');
+        exit();
+    }
+
+
+    $service = new Utils\BackendService(CHAT_SERVER_URL, CHAT_SERVER_ID);
+    
+    
+    // prüfe ob der aktuelle User eine Freundschaftsanfrage gesendet hat
+    $errFriendRequest = "";
+    if(isset($_POST['addFriend'])) {
+        $errFriendRequest = "";
+        $newFriend = $service->loadUser($_POST['addFriend']);
+        if($service->friendRequest($newFriend)) {
+            unset($_POST['addFriend']);
+            $errFriendRequest = "";
+        } else {
+            unset($_POST['addFriend']);
+            $errFriendRequest = "Friend request failed!";
+        } 
+    }
+
+    
+    // Accept/Dismiss handling
+    $errMsg = "";
+    if(isset($_POST['actionRequest'])) {
+        // speichert auf [0] den Username und auf [1] die Aktion
+        $control = explode(':', $_POST['actionRequest']);
+
+        $action = $control[1];
+        $targetUser = $service->loadUser($control[0]);
+        if($action === "accept") {
+            // accept friend
+            $targetUser->setAccepted();
+            $service->friendAccept($targetUser);
+        } else if ($action ===  "dismiss"){
+            // dismiss friend
+            $targetUser->setDismissed();
+            $service->friendDismiss($targetUser);
+        }
+    }
+    
+    // sort users in arrays
+    $userList = $service->loadFriends();
+    
+    $userRequests = array();
+    $friends = array(); 
+    foreach($userList as $user) {
+        if($user->{'status'} === 'accepted') {
+            $friends[] = $user;
+        } else if ($user->{'status'} === 'requested') {
+            $userRequests[] = $user;
+        }
+    }
+
+
 ?>
+<!DOCTYPE html>
+<html>
 <head>
     <title>Friends</title>
     <meta name="viewport" charset="UTF-8" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
@@ -15,76 +73,110 @@
 
     <script src="..\js\prefix-search.js"></script>
     <script>
-        window.chatToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjM2NjU0MzMwfQ.t0AIqQIzxGgIqhPMPUCIFZiiTQE1BpPJEkEe8YsmxSY";
-        window.chatCollectionId = "b5eb31e8-e269-4c8a-9c86-e99c3a992f51";
-        window.chatServer = "https://online-lectures-cs.thi.de/chat";
+        window.chatToken = "<?=$_SESSION['chat_token']?>";
+        window.chatCollectionId = "<?=CHAT_SERVER_ID?>";
+        window.chatServer = "<?=CHAT_SERVER_URL?>";
     </script>
 </head>
 
-<body onload="loadUsers()">
+<body>
     <h1>Friends</h1>
     <p>
         <a href="logout.php">&lt; Logout</a> | <a href="settings.php">Settings</a>
     </p>
     <hr>
     <div class="containerBox">
-        <ul class="friendsList">
-            <li class="friend">
-                <p class="friendContainer">
-                    <span class="friendName">
-                        Tom
-                    </span>
-                    <span class="newMessages">
-                        3
-                    </span>
-                </p>
-            </li>
-
-            <li class="friend">
-                <p class="friendContainer">
-                    <span class="friendName">
-                        Marvin
-                    </span>
-                    <span class="newMessages">
-                        1
-                    </span>
-                </p>
-            </li>
-
-            <li class="friend">
-                <p class="friendContainer">
-                    <span class="friendName">
-                        Tick
-                    </span>
-                    <span class="newMessages">
-                        
-                    </span>
-                </p>
-            </li>
-
-            <li class="friend">
-                <p class="friendContainer">
-                    <span class="friendName">
-                        Trick
-                    </span>
-                    <span class="newMessages">
-                        
-                    </span>
-                </p>
-            </li>
+        <ul class="friendsList"  id="chatList">
+<?php
+    // erstelle die Liste wenn Freunde vorhanden sind
+    if(sizeof($friends) != 0) {
+        foreach($friends as $friend) {
+            if($friend->{'status'} === "accepted") {
+?>
+                <li class="friend">
+                    <!--eventhandler wenn ein chat Partner angeklickt wird-->
+                    <p class="friendContainer" onclick="selectChat('<?=$friend->getUsername()?>')">
+                        <span class="friendName">
+                            <?= $friend->getUsername()?>
+                        </span>
+                        <span class="newMessages">
+                            <?=$service->getUnread($friend->getUsername())?>
+                        </span>
+                    </p>
+                </li>
+<?php
+            }
+        }
+    // wenn keine Freunde in $friends vorhanden sind gib folgendes aus
+    } else {
+?>
+                <li class="friend">
+<?php
+                    echo "You have no friends :'(";
+?>
+                </li>
+<?php
+    }
+?>
         </ul>
     </div>
+
+<?php
+    // wenn Freundschaftsanfragen vorhanden sind gib die Liste aus
+    if(sizeof($userRequests) != 0) {
+?>
+        <hr>
+        <h2>New Requests</h2>
+        <div><?=$errMsg?></div>
+        <form action="friends.php" method="post">
+            <ol>
+<?php
+        foreach($userRequests as $friend) {
+            if($friend->{'status'} === 'requested') {
+?>
+                <li>
+                    <p class="requestContainer">
+                        <span class="request">
+                            Friend request from <strong><?=$friend->getUsername()?></strong>
+                        </span>
+                        <button name="actionRequest" type="submit" value="<?=$friend->getUsername()?>:dismiss" class="formButton requestBtn">Dismiss</button>
+                        <button name="actionRequest" type="submit" value="<?=$friend->getUsername()?>:accept" class="submitButton requestBtn">Accept</button>
+                    </p>
+                </li>
+<?php
+            }
+        }
+?>
+            </ol>
+        </form>
+<?php        
+    }
+?>
+
     <hr>
-    <h2>New Requests</h2>
-    <ol>
-        <li><a href="chat.php">Friend request from <strong>Track</strong></a></li>
-    </ol>
-    <hr>
-    <form class="singleTextForm">
-        <input type="text" id="addFriendFeld" placeholder="Add Friend to List" list="userList" class="formText" autocomplete="off">
-        <button type="submit" class="formButton">Add</button>
+    <div class="dangerLink"><?=$errFriendRequest?></div>
+    <form action="friends.php" method="POST" class="singleTextForm">
+        <input  name="addFriend" type="text" id="addFriendFeld" placeholder="Add Friend to List" 
+                list="userList" class="formText" autocomplete="off">
+        <button name="action" type="submit" class="submitButton">Add</button>
         <datalist id="userList"></datalist>
     </form>
+
+    <script>
+        function selectChat(username) {
+            const li = document.getElementById("chatList");
+            const form = document.createElement("form");
+            form.setAttribute("action", "chat.php");
+            form.setAttribute("method", "get");
+            const input = document.createElement("input");
+            input.setAttribute("type", "hidden");
+            input.setAttribute("value", username);
+            input.setAttribute("name", "chatpartner");
+            form.appendChild(input);
+            li.appendChild(form);
+            form.submit();
+        }
+    </script>
 </body>
 
 </html>
