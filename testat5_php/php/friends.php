20c31cbc255e33e88b515fe66277317e7c236dd7
STRG-C-Coder <81913916+STRG-C-Coder@users.noreply.github.com>
2021-12-20 17:38:07 +0100

    Add files via upload
    fix friends

--- testat5_php/php/friends.php
+++ testat5_php/php/friends.php
@@ -86,7 +86,7 @@
     </script>
 </head>
 
-<body>
+<body onload="">
     <h1>Friends</h1>
     <p>
         <a href="logout.php">&lt; Logout</a> | <a href="settings.php">Settings</a>
@@ -169,21 +169,7 @@
         <datalist id="userList"></datalist>
     </form>
 
-    <script>
-        function selectChat(username) {
-            const li = document.getElementById("chatList");
-            const form = document.createElement("form");
-            form.setAttribute("action", "chat.php");
-            form.setAttribute("method", "get");
-            const input = document.createElement("input");
-            input.setAttribute("type", "hidden");
-            input.setAttribute("value", username);
-            input.setAttribute("name", "chatpartner");
-            form.appendChild(input);
-            li.appendChild(form);
-            form.submit();
-        }
-    </script>
+    <script src="..\js\friends.js"></script>
 </body>
 
 </html>
